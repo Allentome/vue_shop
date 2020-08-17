7418340c671aec3d354b85727dcc09fcef742e84
Allen <610314595@qq.com>
2020-08-17 22:11:06 +0800

    add files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <!-- APP根组件 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+</style>
